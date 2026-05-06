56fd4a4cf66119083e1eefb7702ee0266ca4fd89
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-06 20:44:06 +0600

    Problem 1 solved

---
+++ solutions.ts
@@ -0,0 +1,7 @@
+// Problem 1
+const filterEvenNumbers = (numbers: number[]) => {
+    const duplicateNumber = numbers.filter((num: number) => num % 2 === 0)
+    return duplicateNumber
+}
+
+filterEvenNumbers([1, 2, 3, 4, 5, 6])
