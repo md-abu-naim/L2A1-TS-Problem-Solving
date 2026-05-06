371b9cf0c33e9174bf98ac4d0fbb1bb1638108b7
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-06 22:16:44 +0600

    TS Problem solving complete

--- solutions.ts
+++ solutions.ts
@@ -1,19 +1,14 @@
 // Problem 1
 const filterEvenNumbers = (numbers: number[]) => {
-    const duplicateNumber = numbers.filter((num: number) => num % 2 === 0)
-    return duplicateNumber
+    const evenNumbers = numbers.filter((num: number) => num % 2 === 0)
+    return evenNumbers
 }
-
-filterEvenNumbers([1, 2, 3, 4, 5, 6])
-
 
 // Problem 2
 const reverseString = (str: string) => {
-    const reverse = str.split('').reverse().join('')
-    return reverse
+    const reversed = str.split('').reverse().join('')
+    return reversed
 }
-
-reverseString('typescript')
 
 
 // Problem 3
@@ -22,13 +17,9 @@ type StringOrNumber = string | number
 const checkType = (value: StringOrNumber) => {
     if (typeof value === 'number') {
         return 'Number'
-    } else if (typeof value === 'string') {
-        return 'String'
     }
+    return 'String'
 }
-
-checkType('hellow')
-checkType(52)
 
 
 // Problem 4
@@ -36,9 +27,6 @@ const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
     const result = obj[key]
     return result
 }
-
-const user = { id: 1, name: "John Doe", age: 21 };
-getProperty(user, "id");
 
 
 // Problem 5
@@ -53,16 +41,12 @@ const toggleReadStatus = (book: Book) => {
 }
 
 
-const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
-toggleReadStatus(myBook);
-
-
 // Problem 6
 class Person {
     name: string;
     age: number
 
-    constructor(name: string, age: number){
+    constructor(name: string, age: number) {
         this.name = name
         this.age = age
     }
@@ -71,16 +55,21 @@ class Person {
 class Student extends Person {
     grade: string
 
-    constructor(name: string, age: number, grade: string){
+    constructor(name: string, age: number, grade: string) {
         super(name, age)
 
         this.grade = grade
     }
 
-    getDetails(){
-        console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
     }
 }
 
-const student = new Student("Alice", 20, "A");
-student.getDetails();
+
+
+// Problem 7
+const getIntersection = (arr1: number[], arr2: number[]) => {
+    return arr1.filter((item) => arr2.includes(item))
+
+}
